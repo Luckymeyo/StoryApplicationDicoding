dc3e353c56e3615142e98487decb1e51c794cf74
Luckymeyo <52824270+Luckymeyo@users.noreply.github.com>
2025-04-24 21:07:46 +0700

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -7,10 +7,11 @@ module.exports = {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
+  filename: 'app.bundle.js',
+  path: path.resolve(__dirname, 'dist'),
+  clean: true,
+  publicPath: '/StoryApplicationDicoding/',
+}
   module: {
     rules: [
       {
