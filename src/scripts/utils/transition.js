375942946f1ed2acc1c7a88bac2aba56dc096079
Luckymeyo <52824270+Luckymeyo@users.noreply.github.com>
2025-04-24 20:57:14 +0700

    Delete src directory

--- src/scripts/utils/transition.js
+++
@@ -1,7 +0,0 @@
-export function transitionHelper({ skipTransition = false, updateDOM }) {
-  if (skipTransition || !document.startViewTransition) {
-    const done = Promise.resolve(updateDOM());
-    return { ready: Promise.reject('unsupported'), updateCallbackDone: done, finished: done };
-  }
-  return document.startViewTransition(updateDOM);
-}
--- src/scripts/views/about.js
+++
@@ -1,8 +0,0 @@
-export function renderAbout(container) {
-  container.innerHTML = `
-    <section class="page about-page">
-      <h2>Tentang Aplikasi</h2>
-      <p>Aplikasi ini dibuat untuk menampilkan cerita dari pengguna dengan lokasi di peta menggunakan Leaflet dan MapTiler.</p>
-    </section>
-  `;
-}
